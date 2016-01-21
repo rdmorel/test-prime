debad9a5e78e6366dc9e70a55637f9a59621ccd1
rdmorel <race@morelweb.com>
2016-01-21 01:10:36 -0700

    Update testPrime(n).asm

--- testPrime(n).asm
+++ testPrime(n).asm
@@ -2,9 +2,9 @@
 
 N:	.word 0										#setting variable
 Ni: .word 1										#setting variable
-txt: .ascii "Input of: "			#printing
-br: .asciiz "\n"							#line break
-res: .ascii "Returns: "				#more printing
+txt: .ascii "Input of: "								#printing
+br: .asciiz "\n"									#line break
+res: .ascii "Returns: "									#more printing
 
 .text										    	#alphanumeric identifier
 .globl main										#make global for external use
@@ -16,13 +16,13 @@ main:										    	#setting values
 	li $s3, 1								  	#s3 = 1
 
 
-	jal testPrime								#jump to testPrime, link address
-	jal printing								#go print results
+	jal testPrime									#jump to testPrime, link address
+	jal printing									#go print results
 	
 	lw $s0, Ni									#s0 = N = 29
 
-	jal testPrime								#testPrime again
-	jal printing								#print result
+	jal testPrime									#testPrime again
+	jal printing									#print result
 
 	li $v0, 10 									#referencing exit function
 	syscall									  	#execute exit
@@ -31,22 +31,22 @@ main:										    	#setting values
 
 testPrime:										#loop that finds remainder of N / (all integers < N/2)
 
-	beq $s0, $zero, compound					#if N = 0, not prime
-	beq $s0, $s3, compound						#if N = 1, not prime
+	beq $s0, $zero, compound							#if N = 0, not prime
+	beq $s0, $s3, compound								#if N = 1, not prime
 	
-	srl $s1, $s0, 1							#s1 = N/2
-
+	srl $s1, $s0, 1									#s1 = N/2
+	
 	j loop									  	#jump to loop
 
 loop:										     	#iterate through divisors
 
-	slt $t0, $s1, $s2				  	#if $s0 < 2, $t0 = 1, else $t0 = 0
-	beq $t0, $s3, prime					#if $t0 == 1 we have tried all possible divisors, so jump to prime
+	slt $t0, $s1, $s2				  				#if $s0 < 2, $t0 = 1, else $t0 = 0
+	beq $t0, $s3, prime								#if $t0 == 1 we have tried all possible divisors, so jump to prime
 
-	rem $t1, $s0, $s1						#$t1 = N % $s1  DOES THIS WORK???
+	rem $t1, $s0, $s1								#$t1 = N % $s1
 
-	beq $t1, $zero, compound		#if N % s0 = 0, N is not prime, so jump to compound
-	addi $s1, $s1, -1						#decrement s0
+	beq $t1, $zero, compound							#if N % s0 = 0, N is not prime, so jump to compound
+	addi $s1, $s1, -1								#decrement s0
 	j loop									  	#jump to top of loop
 
 prime:									  		#if prime
@@ -68,7 +68,7 @@ printing:
 	syscall									  	#execute
 
 	li $v0, 1							  		#referencing print int system call
-	move $a0, $s0								#passing value to print int system call
+	move $a0, $s0									#passing value to print int system call
 	syscall									  	#execute print
 
 	li $v0, 4 									#referencing the print_string system call (4)
@@ -81,7 +81,7 @@ printing:
 	syscall									  	#execute
 
 	li $v0, 1								  	#referencing print int system call
-	move $a0, $s4								#passing value to print int system call
+	move $a0, $s4									#passing value to print int system call
 	syscall								  		#execute print
 	
 	li $v0, 4 									#referencing the print_string system call (4)
